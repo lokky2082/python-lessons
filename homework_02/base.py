5fae10bc51c5c6c0ad6a0c83c2034f4a80adafce
lokky2082 <lokky2082@gmail.com>
2023-10-10 20:50:54 +0300

    f

--- homework_02/base.py
+++ homework_02/base.py
@@ -2,11 +2,11 @@ from abc import ABC
 from exceptions import LowFuelError, NotEnoughFuel
 
 class Vehicle(ABC):
-    def __init__(self, weight=100, started=False, fuel=200, fuel_consumption=5):
+    started = False
+    def __init__(self, weight=100, fuel=200, fuel_consumption=5):
         self.weight = weight
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
-        self.started = started
     def start(self):
         if self.started == False:
             if (self.fuel > 0) & (self.fuel > self.fuel_consumption):
@@ -24,7 +24,8 @@ weight = 3254
 fuel = 200
 fuel_consumption = 3
 vehile =  Vehicle(weight, fuel, fuel_consumption)       
-print(vehile.weight)       
+print(vehile.fuel)
+print(vehile.fuel_consumption)    
         
         
         
